8c39a26e6898e822e0c20372cda5c7189fa18bf9
Miguel Angel Femenia <mfemvaz2906@g.educaand.es>
2022-10-19 09:08:43 +0200

    Cambios proyecto Laura

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-    echo "<h1>Hola caracola</h1>";
-    echo "meow ";
-
     if ($_POST) {
         $nombre = $_POST['nombre'];
 
         echo "hola " . $nombre;
     }
+
+    echo "Solo se que no se nada";
+    echo "pero ya se algo, y es que no se nada";
 ?>
